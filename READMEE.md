a22f6984789fdeb38226858a725435473d3bacd5
JulianGreen07 <julianantonioperezg@gmail.com>
2025-04-01 19:33:42 -0400

    Start new respository

--- READMEE.md
+++ READMEE.md
@@ -1,2 +1,1 @@
-# Devops_api1
 # api1jenkins
---
+++ jobscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+nombre="Julian"
+#Empezar el loop
+for a in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
+  #if es igual a 8 que el loop descanse
+  if [ $a == 8 ]
+  then
+    sleep 15
+    echo "A descansar de clase $nombre"
+  fi
+  echo "Clase Nº $a"
+done
+sleep 5
+echo "Bien $nombre, terminamos las clases de $curso, nos vemos."
+
